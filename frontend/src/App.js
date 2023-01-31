a973a9f7271b0d298e66103fbf92d574893e91ff
taikyunn <taichi_141@me.com>
2023-01-31 16:04:20 +0900

    firebaseのデータを取得

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import SampleData from './components/SampleData';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// firebaseの設定
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+console.log(firebaseConfig);
+
+// Firebaseの初期化
+firebase.initializeApp(firebaseConfig);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>FireBaseData</h1>
+          <SampleData />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
